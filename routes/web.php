f0f2136244ff8474b64ab1b5b98291959ffc6f64
Nayru <nanualexandraramirez@gmail.com>
2022-06-01 18:12:41 -0500

    solving controller call

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/nosotros', 'RecetaController@hola');
+use App\Http\Controllers\RecetaController;
+Route::get('/' , [RecetaController::class,'hola']);
+Route::get('/nosotros' , [RecetaController::class,'hola']);
