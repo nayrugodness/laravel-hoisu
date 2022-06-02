4e4803c5d41c5628826ab95b860415b6a383b8af
Nayru <nanualexandraramirez@gmail.com>
2022-06-02 17:59:24 -0500

    adding extends in templates

--- resources/views/recetas/index.blade.php
+++ resources/views/recetas/index.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app');
 
-<h1>Recetas</h1>
+@section('content')
+    <h1>Recetas</h1>
 
-@foreach ( $recetas as $recetas )
-    <li>{{ $recetas }}</li>
-@endforeach
+    @foreach ( $recetas as $recetas )
+        <li>{{ $recetas }}</li>
+    @endforeach
 
-<h2>Categorias</h2>
-@foreach ( $categorias as $categorias )
-    <li>{{ $categorias }}</li>
-@endforeach
+    <h2>Categorias</h2>
+    @foreach ( $categorias as $categorias )
+        <li>{{ $categorias }}</li>
+    @endforeach
+@endsection
