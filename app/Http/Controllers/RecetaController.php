175242343e7e0b33cbe49598506d9b59446a58f4
Nayru <nanualexandraramirez@gmail.com>
2022-06-01 19:49:19 -0500

    creating folder recetas

---
+++ app/Http/Controllers/RecetaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecetaController extends Controller
+{
+    public function hola(){
+        return "funciona";
+    }
+}
