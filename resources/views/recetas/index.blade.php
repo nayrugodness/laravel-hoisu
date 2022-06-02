fdd1ac7438b7c44c6ccc93fafaeeacb51c36a4f2
Nayru <nanualexandraramirez@gmail.com>
2022-06-01 20:11:00 -0500

    printing variables in template

--- resources/views/recetas/index.blade.php
+++ resources/views/recetas/index.blade.php
@@ -3,3 +3,8 @@
 @foreach ( $recetas as $recetas )
     <li>{{ $recetas }}</li>
 @endforeach
+
+<h2>Categorias</h2>
+@foreach ( $categorias as $categorias )
+    <li>{{ $categorias }}</li>
+@endforeach
