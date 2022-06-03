9d4d6930c4b516f2d238b9f8811c7c1abc86391f
Nayru <nanualexandraramirez@gmail.com>
2022-06-03 09:27:54 -0500

    adding table categoria_receta

--- database/migrations/2022_06_03_002154_create_recetas_table.php
+++ database/migrations/2022_06_03_002154_create_recetas_table.php
@@ -14,6 +14,13 @@ class CreateRecetasTable extends Migration
      */
     public function up()
     {
+
+        Schema::create('categoria_receta', function(Blueprint $table ){
+            $table->id();
+            $table->string('nombre');
+            $table->timestamps();
+        });
+
         Schema::create('recetas', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
