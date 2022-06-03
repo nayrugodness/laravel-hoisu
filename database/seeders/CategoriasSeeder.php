06598dd1892664d4d3845a11eefb2a1fad2e6627
Nayru <nanualexandraramirez@gmail.com>
2022-06-03 09:51:40 -0500

    creating more seeds

--- database/seeders/CategoriasSeeder.php
+++ database/seeders/CategoriasSeeder.php
@@ -21,5 +21,26 @@ class CategoriasSeeder extends Seeder
                 'updated_at'=>date('Y-m-d H:i:s')
                 ]
         );
+        DB::table('categoria_receta')->insert(
+            [
+                'nombre'=>'Comida Italiana',
+                'created_at'=>date('Y-m-d H:i:s'),
+                'updated_at'=>date('Y-m-d H:i:s')
+                ]
+        );
+        DB::table('categoria_receta')->insert(
+            [
+                'nombre'=>'Comida Peruana',
+                'created_at'=>date('Y-m-d H:i:s'),
+                'updated_at'=>date('Y-m-d H:i:s')
+                ]
+        );
+        DB::table('categoria_receta')->insert(
+            [
+                'nombre'=>'Comida Francesa',
+                'created_at'=>date('Y-m-d H:i:s'),
+                'updated_at'=>date('Y-m-d H:i:s')
+                ]
+        );
     }
 }
